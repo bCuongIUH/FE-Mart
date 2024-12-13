d40d1b0c6ae31307f8f7efdc86b63e03b3fa2d75
bCuongIUH <bachcuong27@gmail.com>
2024-12-13 08:40:38 +0700

    update deploy

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/categories"; // URL API cho categories
-
+// const API_URL = "http://localhost:5000/api/categories"; 
+const API_URL = "https://cmartshopbe.onrender.com/categories"; 
 // Cấu hình cho axios với token từ localStorage
 const config = {
   withCredentials: true,
